29baba7dc95a7224aea190c7c9ecc188bb859cd8
ivan_bal <semenov.siberian@gmail.com>
2020-09-14 15:24:46 +0700

    add carousel

--- app/js/main.dev.js
+++ app/js/main.dev.js
@@ -1,16 +1,16 @@
+"use strict";
+
 $(document).ready(function () {
-  
   var headerSlider = $(".owl-carousel");
   headerSlider.owlCarousel({
     items: 1,
     loop: true,
     dots: false
   });
-
   $('#headerSliderRight').click(function () {
     headerSlider.trigger('next.owl.carousel');
-  })
+  });
   $('#headerSliderLeft').click(function () {
     headerSlider.trigger('prev.owl.carousel');
-  })
+  });
 });
